ee98d8e67f066ef9c9052f92910b922b7a7a476c
Abk <eng.abdullahkhisraw@gmail.com>
2021-08-12 18:55:15 +0200

    Created the normal distribution function, and the plot option.

--- PyWinda/dfm_module.py
+++ PyWinda/dfm_module.py
@@ -1,6 +1,8 @@
 from PyWinda import pywinda as pw
+from PyWinda import pwploter
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 
 
@@ -26,7 +28,7 @@ def number_of_simsulaions(standard_error_of_mean,confidence_level,standard_norma
 
 
 
-def normal_dist(mean,sd,a=0,b=0,num=1000):
+def normal_dist(mean,sd,a=0,b=0,num=100,plot=False):
     """
         This function generates the normal (Gaussian) distribution, for a given mean value and standard distribution in an interval of a to b or a default of -5*standard deviation to 5*standard defviations.
 
@@ -34,40 +36,34 @@ def normal_dist(mean,sd,a=0,b=0,num=1000):
         :param sd: [*req*]  the standard deviation of the distribution
         :parm a: [*req*] the start of the interval
         :param b: [*req*] the end of the interval
+        :param num: [*optional*] resolution of the interval.
+        :return x,y, plot: x the 1D-array of considered interval, and y the 1D-array of corresponding probability values.
 
     """
+
     a=mean-5*sd
     b=mean+5*sd
-    interval=np.linspace(a,b,num) #creates the interval with the default number of seperation, here 100.
+    x=np.linspace(a,b,num) #creates the interval with the default number of seperation, here 100.
     y=[] #the normal distribution values
-    for x in interval:
-        y.append((1/(sd*np.sqrt(2*np.pi)))*np.exp(-0.5*((x-mean)/sd)**2))
-    return y
+    for i in x:
+        y.append((1/(sd*np.sqrt(2*np.pi)))*np.exp(-0.5*((i-mean)/sd)**2))
+
+    if plot ==True:
+        figure,ax=pwploter.plotg(x,y,title="Normal Distribution",xlabel='Values [-]',ylabel='Probability [-]',text={'mean':mean,'Standard deviation':sd})
+        return y, x, figure # returns y and x and the final figure if plot
+
+    return y,x  #returns y and x if not plot is done
+
+
+
 
 ###################################
 #####The drafts section ###########
-normal_dist(253,1)
-print(np.exp(1))
+
+a,b,fig=normal_dist(15,1,plot=True)
+plt.show()
 
 
 
-
-# x=np.random.uniform(3,6,100)
-
-# y=np.random.triangular(3,6,100,500)
-# a=0
-# for x in y:
-#     if x==6:
-#         a=a+1
-#     else:
-#         print(x)
-# print(a)
-# print(a/50)
-# print(y)
-
-
-# h = plt.hist(np.random.triangular(-3, 0, 8, 100000), bins=200,
-#              density=True)
-# plt.show()
 #######The drafts section ends here###########
 ##############################################
