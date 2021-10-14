eea84ba969288be45950ced4d1dc828de1eb06c8
Abk <eng.abdullahkhisraw@gmail.com>
2021-10-14 17:20:02 +0200

    NumPy generators were integrated to generate random numbers for all the distributions introduced before.

--- PyWinda/dfm_module.py
+++ PyWinda/dfm_module.py
@@ -1,10 +1,17 @@
 import pwploter #only for documentation
-# from PyWinda import pwploter # only for pytest
+# from PyWinda import pwploter # for default and for pypi
 import numpy as np
+from numpy.random import default_rng
 import math
 import matplotlib.pyplot as plt
 
 
+pgreen1='#009C86'
+pgreen2='#0397A1'
+pblue='#05354B'
+pred='#96394E'
+ppink='#ECA48E'
+pyellow='#ECD384'
 
 
 def number_of_simsulaions(standard_error_of_mean,confidence_level,standard_normal_statistics,standard_deviaton_of_the_output):
@@ -13,124 +20,114 @@ def number_of_simsulaions(standard_error_of_mean,confidence_level,standard_norma
 
 
 
-def normal_dist(mean,sd,a=0,b=0,num=100,plot=False):
+def normal_dist(mean,sd,num=1000,plot=False,seed=None,bins=100):
     """
-        This function generates the normal (Gaussian) distribution, for a given mean value and standard distribution in an interval of a to b or a default of -5*standard deviation to 5*standard defiations.
+        This function generates the normal (Gaussian) distribution, for a given mean value and standard deviation.
 
         :param mean: [*req*] the mean of the distribution
         :param sd: [*req*]  the standard deviation of the distribution
-        :param a: [*optional*] the start of the interval of the possible values
-        :param b: [*optional*] the end of the interval of the possible values
-        :param num: [*optional*] resolution of the interval.
-        :return x,y, figure: x is the 1D-array of considered interval, y is the 1D-array of corresponding probability values, and if the plot option is set to True a figure will also be outputed.
+        :param num: [*optional*] number of samples, by default 1000.
+        :param plot: [*optional*] if True a figure will also be returned.
+        :param seed: [*optional*] use the same seed number to produce the same distribution.
+        :param bins: [*optional*] number of bins to divide the number of samples to. Default value is 100.
+        :return s,counts,bins, fig: s is the 1D-array of generated samples, counts is the 1D-array of corresponding probability values to different bins, bins is the 1D-array of bins edges, and fig is to retrieve generated figure, only if plot is set to True.
 
         :Example:
 
                     >>> from PyWinda import pywinda as pw
-                    >>> y,x,fig=normal_dist(mean=20,sd=2,plot=True)
-                    >>> print(y) #doctest:+ELLIPSIS
-                    [7.433597573671488e-07, 1.2255305214711653e-06, 1.999945186208313e-06, ...]
-                    >>> print(x)
-                    [10.         10.2020202  10.4040404  10.60606061 10.80808081 11.01010101
-                     11.21212121 11.41414141 11.61616162 11.81818182 12.02020202 12.22222222
-                     12.42424242 12.62626263 12.82828283 13.03030303 13.23232323 13.43434343
-                     13.63636364 13.83838384 14.04040404 14.24242424 14.44444444 14.64646465
-                     14.84848485 15.05050505 15.25252525 15.45454545 15.65656566 15.85858586
-                     16.06060606 16.26262626 16.46464646 16.66666667 16.86868687 17.07070707
-                     17.27272727 17.47474747 17.67676768 17.87878788 18.08080808 18.28282828
-                     18.48484848 18.68686869 18.88888889 19.09090909 19.29292929 19.49494949
-                     19.6969697  19.8989899  20.1010101  20.3030303  20.50505051 20.70707071
-                     20.90909091 21.11111111 21.31313131 21.51515152 21.71717172 21.91919192
-                     22.12121212 22.32323232 22.52525253 22.72727273 22.92929293 23.13131313
-                     23.33333333 23.53535354 23.73737374 23.93939394 24.14141414 24.34343434
-                     24.54545455 24.74747475 24.94949495 25.15151515 25.35353535 25.55555556
-                     25.75757576 25.95959596 26.16161616 26.36363636 26.56565657 26.76767677
-                     26.96969697 27.17171717 27.37373737 27.57575758 27.77777778 27.97979798
-                     28.18181818 28.38383838 28.58585859 28.78787879 28.98989899 29.19191919
-                     29.39393939 29.5959596  29.7979798  30.        ]
-                     >>> print(fig)
-                     Figure(1000x800)
+                    >>> samples,pr,bins,fig=normal_dist(mean=20,sd=2,plot=True,seed=12)
+                    >>> print(samples) #doctest:+ELLIPSIS
+                    [19.98634644 22.09228658 21.48317684 21.44791308 ...]
+                    >>> print(fig)
+                    Figure(1000x800)
 
 
         \\----------------------------------------------------------------------------------------------------------------------------------------------------------
 
     """
-    if a==0: a=mean-5*sd
-    if b==0: b=mean+5*sd
-    x=np.linspace(a,b,num) #creates the interval with the default number of seperation, here 100.
-    y=[] #the normal distribution values
-    for i in x:
-        y.append((1/(sd*np.sqrt(2*np.pi)))*np.exp(-0.5*((i-mean)/sd)**2))
-    if plot ==True:
-        figure,ax=pwploter.plot(x,y,title="Normal Distribution",xlabel='Values [-]',ylabel='Probability [-]',text={'mean':mean,'Standard deviation':sd})
-        return y, x, figure # returns y and x and the final figure if plot
-    elif plot==False:
-        return y,x  #returns y and x if not plot is done
+
+    rng=default_rng(seed) #the default random number generator
+    y=rng.normal(loc=mean, scale=sd, size=num)
+    figure, ax, counts, bins_array = pwploter.hist(y, bins=bins, title="Normal Distribution", xlabel='Values [-]',
+                                                   ylabel='Probability [-]',
+                                                   text={'mean': mean, 'Standard deviation': sd,
+                                                         'Number of samples': num, 'Bins': bins})
+    if plot:
+        ax.plot(bins_array, 1 / (sd * np.sqrt(2 * np.pi)) *
+             np.exp(- (bins_array - mean) ** 2 / (2 * sd ** 2)),
+             linewidth=2, color=pred)
+        return y,counts,bins_array, figure # returns samples counts in bins, bins array and the figure.
+    else:
+        return y, counts, bins_array
 
 
-
-
-def cdf_normal(mean, sd, x,a=0,b=0,plot=False):
+def cdf_normal(mean,sd,x=None,num=1000,plot=False,seed=None,bins=100):
     """
         This function generates the cumulative distribution function of the normal distributin function with the given parameters.
 
-        :param mean: [*req*] the mean of the normal distribution
-        :param sd: [*req*]  the standard deviation of the normal distribution
-        :param x: [*req*]  any value within +/-5 times standard deviation.
-        :param a: [*optional*] the start of the interval of the possible values
-        :param b: [*optional*] the end of the interval of the possible values
-        :param num: [*optional*] resolution of the interval.
-        :return CDF,values, fig: values is the 1D-array of considered interval from 0 to x, CDF is the 1D-array of corresponding cumulative probability values, and if the plot option is set to True a figure will also be outputed.
-
+        :param mean: [*req*] the mean of the distribution
+        :param sd: [*req*]  the standard deviation of the distribution
+        :param x: [*optional*]  if any value is given the cdf will be generated only upto x.
+        :param num: [*optional*] number of samples, by default 1000.
+        :param plot: [*optional*] if True a figure will also be returned.
+        :param seed: [*optional*] use the same seed number to produce the same distribution.
+        :param bins: [*optional*] number of bins to divide the number of samples to. Default value is 100.
+        :return s,cumSum,bins, fig: s is the 1D-array of generated values, cumSum is the 1D-array of calculated cumulative probability values, bins is the 1D-array of bins edges, and fig is to retrieve generated figure, only if plot is set to True.
 
         :Example:
 
                     >>> from PyWinda import pywinda as pw
-                    >>> cdf,x,fig=cdf_normal(mean=0,sd=1,x=1,plot=True)
+                    >>> s,cdf,bins,fig=cdf_normal(mean=0,sd=1,x=1,plot=True,seed=145)
                     >>> print(cdf) #doctest:+ELLIPSIS
-                    [2.8665157186802404e-07, 4.816529797224689e-07, ...]
-                    >>> print(x)
-                    [-5.         -4.8989899  -4.7979798  -4.6969697  -4.5959596  -4.49494949
-                     -4.39393939 -4.29292929 -4.19191919 -4.09090909 -3.98989899 -3.88888889
-                     -3.78787879 -3.68686869 -3.58585859 -3.48484848 -3.38383838 -3.28282828
-                     -3.18181818 -3.08080808 -2.97979798 -2.87878788 -2.77777778 -2.67676768
-                     -2.57575758 -2.47474747 -2.37373737 -2.27272727 -2.17171717 -2.07070707
-                     -1.96969697 -1.86868687 -1.76767677 -1.66666667 -1.56565657 -1.46464646
-                     -1.36363636 -1.26262626 -1.16161616 -1.06060606 -0.95959596 -0.85858586
-                     -0.75757576 -0.65656566 -0.55555556 -0.45454545 -0.35353535 -0.25252525
-                     -0.15151515 -0.05050505  0.05050505  0.15151515  0.25252525  0.35353535
-                      0.45454545  0.55555556  0.65656566  0.75757576  0.85858586  0.95959596
-                      1.        ]
-                     >>> print(fig)
-                     Figure(1000x800)
+                    [0.001      0.001      0.001      0.001      0.001      0.001...]
+                    >>> print(fig)
+                    Figure(1000x800)
 
 
         \\----------------------------------------------------------------------------------------------------------------------------------------------------------
 
     """
-
-
-
-
-
-    ys,xs=normal_dist(mean,sd,a=a,b=b,num=100) #creates the normal distribution with the given mean and standard deviation.
-    if x>=xs[0] and x<=xs[-1]: #checks whether the requested cdf from 0 to x is within the considered range of normal distribution. The normal distribution function of pywinda plots the distributino for +-5 times the standard deviation, more than 99.7% of all the values lie within this range.
+    rng = default_rng(seed)  # the default random number generator
+    y = rng.normal(loc=mean, scale=sd, size=num)
+    hist, bin_edges = np.histogram(y, bins=bins, density=True) #outputs all the number of outputs in every bin in density form.
+    dx = bin_edges[-1] - bin_edges[-2] #dx is constant always
+    cumSum = np.cumsum(hist)
+    cumSum = cumSum * dx
+    if x!=None and x>=bin_edges.min() and x<=bin_edges.max(): #checks whether the requested cdf from 0 to x is within the considered range of normal distribution.
         loc=None #location of the xs greater than or equal to x, finds where the x is located in the given range
-        for ind,val in enumerate(xs):
+        for ind,val in enumerate(bin_edges):
             if val>=x:
                 loc=ind
                 break
-        cdf=[]
-        for i in range(loc):
-            cdf.append(0.5*(1+math.erf((xs[i]-mean)/(sd*math.sqrt(2))))) #used the error function from math module of python.
-        cdf.append(0.5*(1+math.erf((x-mean)/(sd*math.sqrt(2)))))
-        final_xs=xs[0:loc]
-        final_xs=np.append(final_xs,x)
+        xs=bin_edges[0:loc] #truncates only the portion of bin_edges less than x. element at loc is not included.
+        xs=np.array(xs)
+        final_xs=np.append(xs,x) #x is added to the end of interval
+        cumSum_temp=cumSum[0:loc] #truncates only the potin of cumSum for all number less than x. element at loc is not inlcuded.
+        lastelement=cumSum_temp[-1]+(bin_edges[loc]-x)* 1 / (sd * np.sqrt(2 * np.pi)) *np.exp(- (x - mean) ** 2 / (2 * sd ** 2))
+        final_cumSum=np.append(cumSum_temp,lastelement)
+        y_sorted=np.sort(y)
+        loc_y=None
+        for index,value in enumerate(y_sorted):
+            if value>=x:
+                loc_y=index
+                break
+        portion_of_y=y_sorted[:loc_y]
         if plot ==True:
-            figure,ax=pwploter.plot(final_xs,cdf,title="Cumulative Distribution Function",xlabel='Values [-]',ylabel='Probability [-]',text={'mean':mean,'Standard deviation':sd})
-            return cdf, final_xs, figure # returns y and x and the final figure if plot
+            figure, ax = pwploter.plot(final_xs, final_cumSum, title="Normal Distribution", xlabel='Values [-]',ylabel='Probability [-]',
+                                       text={'mean': mean, 'Standard deviation': sd, 'Number of samples': num,
+                                             'Bins': bins, 'x':x})
+            return portion_of_y,final_cumSum,final_xs, figure  # returns y and x and the final figure if plot
         elif plot==False:
-            return cdf,final_xs
+            return portion_of_y,cumSum,bin_edges  #returns y and x if not plot is done
+    elif x==None and plot==True:
+        figure, ax = pwploter.plot(bin_edges[:-1], cumSum, title="Normal Distribution", xlabel='Values [-]',
+                                   ylabel='Probability [-]',
+                                   text={'mean': mean, 'Standard deviation': sd, 'Number of samples': num,
+                                         'Bins': bins})
+        return y,cumSum,bin_edges ,figure  # returns y and x and the final figure if plot
+
+    elif x==None and plot==False:
+        return y,cumSum,bin_edges
     else: # if the x is outside the +-5 times the standard deviation.
         raise Exception ("Out of range, please make sure that the point of interest x is with the range of -+5 times the standard deviation.")
 
@@ -142,10 +139,9 @@ def cdf_normal(mean, sd, x,a=0,b=0,plot=False):
 
 
 
+
 ###################################
 #####The drafts section ###########
-# cdf,x=cdf_normal(0,1,5)
-# cdf1,x=cdf_normal(0,1,-5)
-# print(max(cdf1))
+
 #######The drafts section ends here###########
 ##############################################
--- PyWinda/pwploter.py
+++ PyWinda/pwploter.py
@@ -1,6 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib
+# Say, "the default sans-serif font is COMIC SANS"
+matplotlib.rcParams['font.sans-serif'] = "Times New Roman"
+# Then, "ALWAYS use sans-serif fonts"
+matplotlib.rcParams['font.family'] = "sans-serif"
+# Pywinda color codes:
 
+pgreen1='#009C86'
+pgreen2='#0397A1'
+pblue='#05354B'
+pred='#96394E'
+ppink='#ECA48E'
+pyellow='#ECD384'
 
 
 def plot(x,y,**plt_kwargs):
@@ -18,7 +30,7 @@ def plot(x,y,**plt_kwargs):
             text = str(key)+' = '+str(value)+'\n'
             textstr=textstr+text
         textstr=textstr.rstrip("\n")
-        props = dict(boxstyle='round', facecolor='gray', alpha=0.6)
+        props = dict(boxstyle='round', facecolor=pgreen1, alpha=0.6)
         ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=12,verticalalignment='top', bbox=props,fontname='Times New Roman')
 
     ax.plot(x,y)
@@ -42,3 +54,24 @@ def plot(x,y,**plt_kwargs):
 
     return fig,ax
 
+
+def hist(x,bins,density=True,**plt_kwargs):
+    fig,ax=plt.subplots(figsize=(10,8))
+
+    if 'title' in plt_kwargs:
+        ax.set_title(plt_kwargs['title'], fontsize=16, fontname='Times New Roman')
+    if 'xlabel' in plt_kwargs:
+        ax.set_xlabel(plt_kwargs['xlabel'], fontname='Times New Roman')
+    if 'ylabel' in plt_kwargs:
+        ax.set_ylabel(plt_kwargs['ylabel'], fontname='Times New Roman')
+    if 'text' in plt_kwargs:
+        textstr=''  #making the text option available for pwploter
+        for key, value in plt_kwargs['text'].items():
+            text = str(key)+' = '+str(value)+'\n'
+            textstr=textstr+text
+        textstr=textstr.rstrip("\n")
+        props = dict(boxstyle='round', facecolor=pgreen1, alpha=0.6,edgecolor=pgreen1)
+        ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=12,verticalalignment='top', bbox=props,fontname='Times New Roman')
+
+    counts,bins2,ignored=ax.hist(x,bins,density=density,edgecolor='white')
+    return fig,ax,counts,bins2
